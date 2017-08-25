c85ff9584698b42fc11cf63692add8f1e2e65ebc
J Montag <jmontagdev@gmail.com>
2017-08-25 22:52:58 +0000

    addit edit-thing view and a link to it

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from collection.forms import ThingForm
 from collection.models import Thing
 
 def index(request): 
@@ -16,6 +18,30 @@ def thing_detail(request, slug):
         'thing': thing,
     })
 
+def edit_thing(request, slug):
+    # grab the object
+    thing = Thing.objects.get(slug=slug)
+    # set the form we're using
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form
+    if request.method == 'POST':
+        # grab the data from the submitted form and apply to
+        # the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
+    })
 
 
 
