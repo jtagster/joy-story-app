c84f89cfd99433527aea434558e27dc7b7480d66
J Montag <jmontagdev@gmail.com>
2017-08-25 13:01:10 +0000

    added queryset to display things

--- collection/views.py
+++ collection/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 def index(request): 
-    number = 6
+    things = Thing.objects.all()
     # this is your new view - urls.py will catch that someone wants the homepage 
     #and points to this piece of code, which will render the index.html template.
-    return render(request, 'index.html', {'number':number,})
+    return render(request, 'index.html', {'things': things,})
 
 
 
