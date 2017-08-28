5eb5a1d6c2a619a35eb95630b8f77030590fa64c
J Montag <jmontagdev@gmail.com>
2017-08-28 14:09:52 +0000

    added search/browse and protected editing to only objects user

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-
+from django.template.defaultfilters import slugify
 from collection.forms import ThingForm
 from collection.models import Thing
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 def index(request): 
     things = Thing.objects.all()
@@ -18,16 +20,52 @@ def thing_detail(request, slug):
         'thing': thing,
     })
 
-def edit_thing(request, slug):
-    # grab the object
-    thing = Thing.objects.get(slug=slug)
-    # set the form we're using
+def create_thing(request):
     form_class = ThingForm
 
-    # if we're coming to this view from a submitted form
+    # if we're coming from a submitted form, do this
     if request.method == 'POST':
-        # grab the data from the submitted form and apply to
-        # the form
+        # grab the data from the submitted form and
+        # apply to the form
+        form = form_class(request.POST)
+        if form.is_valid():
+            # create an instance but don't save yet
+            thing = form.save(commit=False)
+
+            # set the additional details
+            thing.user = request.user
+            thing.slug = slugify(thing.name)
+
+            # save the object
+            thing.save()
+
+            # redirect to our newly created thing
+            return redirect('thing_detail', slug=thing.slug)
+
+    # otherwise just create the form
+    else:
+        form = form_class()
+
+    return render(request, 'things/create_thing.html', {
+        'form': form,
+    })
+@login_required
+def edit_thing(request, slug):
+    # grab the object...
+    thing = Thing.objects.get(slug=slug)
+
+    # make sure the logged in user is the owner of the thing
+    if thing.user != request.user:
+        raise Http404
+
+    # set the form we're using...
+    form_class = ThingForm
+
+    # if we're coming to this view from a submitted form,
+    # do this
+    if request.method == 'POST':
+        # grab the data from the submitted form and
+        # apply to the form
         form = form_class(data=request.POST, instance=thing)
         if form.is_valid():
             # save the new data
@@ -43,8 +81,18 @@ def edit_thing(request, slug):
         'form': form,
     })
 
+def browse_by_name(request, initial=None):
+    if initial:
+        things = Thing.objects.filter(name__istartswith=initial)
+        things = things.order_by('name')
+    else:
+        things = Thing.objects.all().order_by('name')
 
-
+    return render(request, 'search/search.html', {
+        'things': things,
+        'initial': initial,
+    })
+    
 """
 There are a ton of different ways to display a template simply in the views, 
 and my favorite is Django's shortcut function called render. This is something 
--- myhellowebapp/urls.py
+++ myhellowebapp/urls.py
@@ -1,9 +1,13 @@
+from collection.backends import MyRegistrationView
 from django.conf.urls import url, include
 from django.contrib.auth.views import (
    password_reset, 
    password_reset_done,
    password_reset_confirm,
    password_reset_complete
+)
+from django.views.generic import (TemplateView, 
+    RedirectView,
 )
 
 from django.contrib import admin
@@ -19,11 +23,17 @@ urlpatterns = [
     url(r'^contact/$', 
         TemplateView.as_view(template_name='contact.html'),
         name='contact'),
+    url(r'^things/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
     url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, 
         name='thing_detail'),    
     url(r'^things/(?P<slug>[-\w]+)/edit/$', 
         views.edit_thing,
         name='edit_thing'),
+    url(r'^browse/$', RedirectView.as_view(pattern_name='browse', permanent=True)),
+    url(r'^browse/name/$',
+        views.browse_by_name, name='browse'),
+    url(r'^browse/name/(?P<initial>[-\w]+)/$', 
+        views.browse_by_name, name='browse_by_name'),      
     url(r'^accounts/password/reset/$', 
         password_reset,
         {'template_name':
@@ -44,6 +54,11 @@ urlpatterns = [
         {'template_name':
         'registration/password_reset_complete.html'},
         name="password_reset_complete"),
+    url(r'^accounts/register/$', 
+        MyRegistrationView.as_view(),
+        name='registration_register'),
+    url(r'^accounts/create_thing/$', views.create_thing, 
+        name='registration_create_thing'),
     url(r'^accounts/', 
         include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
