8195fc9f5ee584c4cfc80c04811988fcc6d550e1
jtagster <jlmontag@gmail.com>
2017-08-31 18:24:49 +0000

    changed allowed_hosts

--- myhellowebapp/settings.py
+++ myhellowebapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yko&g01+w7y)5f8$rbp@0v(k$9wnpv)^f5v!1vo7h!ptq9_)dz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
