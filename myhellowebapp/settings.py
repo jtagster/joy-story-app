9a1a995ac65c478bf0af88f9df47b0b06ebbedba
jtagster <jlmontag@gmail.com>
2017-09-10 17:26:01 +0000

    switch back to sqlite

--- myhellowebapp/settings.py
+++ myhellowebapp/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'myhellowebapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'joystoryapp',
@@ -90,13 +90,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-}
-'''DATABASES = {
+}'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
