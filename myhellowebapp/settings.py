503f3a342ec21a018702ca20ca0776ceadb2cf61
jtagster <jlmontag@gmail.com>
2017-09-14 18:19:06 +0000

    debugging...

--- myhellowebapp/settings.py
+++ myhellowebapp/settings.py
@@ -26,7 +26,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'yko&g01+w7y)5f8$rbp@0v(k$9wnpv)^f5v!1vo7h!ptq9_)dz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #change this before deploying
 ALLOWED_HOSTS = ['www.myjoystory.com', ]
